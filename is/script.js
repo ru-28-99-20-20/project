fb8ac7dfd34f03bf1f066d3268ed1e8518d0b760
Ruslana <sirotiukruslana@gmail.com>
2021-07-17 14:28:45 +0300

    homwork2

--- is/script.js
+++ is/script.js
@@ -9,13 +9,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a  = prompt("Один из последних просмотрегых фильмов?", ""),
-      d  = prompt("На склдько его оцените?", ""),
-      b = prompt("Один из последних просмотрегых фильмов?", ""),
-      c  = prompt("На склдько его оцените?", "");
-
-personalMovieDB.movie [a] = b;
-personalMovieDB.movie [b] = c;
+for( let i = 0; i < 2; i++){
+    const  a  = prompt("Один из последних просмотрегых фильмов?", ""),
+           d  = prompt("На склдько его оцените?", "");
+    if (a != null && d != null && a !="" && d != "" && a.length < 50){
+      personalMovieDB.movie [a] = d;  
+      console.log('done');
+    }else{
+        console.log('error')
+    i--;
+    }
+    
+}
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+}else if(10 >= personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+}else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+}else {
+    console.log('Произошла ошибка');
+}
 console.log(personalMovieDB);
 
 
